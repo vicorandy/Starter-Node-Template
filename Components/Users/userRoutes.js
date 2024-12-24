673cf4eee6b9c3ae23da36feca5b9182c55d19a7
vicorandy <vicorandyrichard26@gmail.com>
2024-12-24 13:35:50 +0100

    first commit

---
+++ Components/Users/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {
+  signUp,
+  signIn,
+
+} = require('./userController');
+
+const userRouter = express.Router();
+
+userRouter.route('/signup').post(signUp);
+userRouter.route('/signin').post(signIn);
+
+
+module.exports = userRouter;
---
+++ app.js
@@ -0,0 +1,25 @@
+require('express-async-errors');
+require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const express = require('express');
+const entriesRouter = require('./Components/Entries/entriesRoutes');
+const usersRouter = require('./Components/Users/userRoutes');
+const notFoundError = require('./MiddelWare/notFound');
+
+const { ENTRIES_URI, USERS_URI } = process.env;
+
+const app = express();
+
+// request body paser
+app.use(express.json());
+
+// swagger documentation
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
+app.use(USERS_URI, usersRouter);
+app.use(notFoundError);
+
+module.exports = app;
